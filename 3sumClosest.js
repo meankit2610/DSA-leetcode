9845bf38a4f2381fc248f4e677792d841d8c3ccc
meankit2610 <ankitsingh5282770@gmail.com>
2023-08-18 22:20:38 +0530

    3sum-closest

---
+++ 3sumClosest.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function (nums, target) {
+  let result = nums[0] + nums[1] + nums[nums.length - 1];
+  nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length - 2; i++) {
+    let start = i + 1,
+      end = nums.length - 1;
+
+    while (start < end) {
+      let sum = nums[i] + nums[start] + nums[end];
+
+      if (sum < target) {
+        start++;
+      } else {
+        end--;
+      }
+      if (Math.abs(sum - target) < Math.abs(result - target)) {
+        result = sum;
+      }
+    }
+  }
+  return result;
+};
